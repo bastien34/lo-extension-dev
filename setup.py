e21471cf50cc0cd2a8544a9dfecaff76cd05fcb1
Bastien Roques <bastien.roques1@gmail.com>
2022-09-23 12:33:17 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,60 @@
+"""
+"""
+
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+
+def read(rel_path: str) -> str:
+    return (here / rel_path).read_text(encoding="utf-8")
+
+
+def get_version(rel_path: str) -> str:
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
+
+long_description = read("README.rst")
+
+setup(
+    name="lo-extension-dev",
+    version=get_version('lo-extension/__init__.py'),
+    description="A tool to help developing LibreOffice Extension.",  # Optional
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/bastien34/lo-extension",
+    author="Bastien Roques",
+    classifiers=[
+        "Production/Stable Status :: 5",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    keywords="extension, libreoffice, development",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.7, <4",
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'lo-extension=lo-extension:main',
+        ],
+    },
+    project_urls={  # Optional
+        "Bug Reports": "https://github.com/bastien34/lo-extension/issues",
+        "Funding": "https://donate.pypi.org",
+        "Source": "https://github.com/bastien34/lo-extension",
+    },
+)
---
+++ src/lo-extension-dev/__init__.py
@@ -0,0 +1,5 @@
+from lo_extension.manage import Extension
+
+__version__='0.0.1'
+
+
